11f0001a3fedb46ef52db9fed014f296d2af9c76
koyamayuki <ukey1024@gmail.com>
2024-03-03 22:41:12 +0900

    タイマー作りかけ

--- main.dart
+++ main.dart
@@ -2,37 +2,33 @@ import 'dart:io';
 import 'dart:async';
 
 void main() {
-  String? oder = stdin.readLineSync();
-  int n = 0;
-  bool flag = true;
-  Timer timer;
-  timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      n = n+1;
-      });//ここがないとなぜかtimer.cancel();でエラーが起きる
-
   print("ストップウォッチを開始するならsを押してね");
   print('時間を知りたいときはtを押してね');
-
-  if(oder == 's'){
-    if(flag == true){
-      timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      n = n+1;
-      });
-    flag = false;
+  String? oder;
+  int n = 0;
+  bool flag = true;
+  // Timer timer;
+  // timer = Timer.periodic(Duration(seconds: 1), (timer) {});//ここがないとなぜかtimer.cancel();でエラーが起きる
+  while(oder != 'end'){
+    oder = stdin.readLineSync();
+    if(oder == 's'){
+      if(flag == true){
+        Timer.periodic(Duration(seconds: 1), (timer) {
+        n = n+1;
+        });
+      flag = false;
+      }
+      // else{
+      //   timer.cancel();
+      //   flag = true;
+      //   print(n);
+      // }
+    }else if(oder == 't'){
+      print(n);
+    }else{
+      print('もう一度入力しなおしてください');
     }
-    else{
-      timer.cancel();
-      flag = true;
-    }
-  }else if(oder == 't'){
-    print(n);
-  }else if(oder == 'end'){
-    print('end');
-  }else{
-    print('もう一度入力しなおしてください');
   }
-  oder = stdin.readLineSync();
-  // print('停止したいときはもう一度sを押してね');
-  // print('リセットしたいときはrを押してね');
-  // print("The entered name is ${inputstart}");
+  print('終了');
+  exit(0); 
 }
