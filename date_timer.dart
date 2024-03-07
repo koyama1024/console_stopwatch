dc1cc39cf49aa306f109041b34be00439e0fc2d9
koyamayuki <ukey1024@gmail.com>
2024-03-07 14:53:17 +0900

    date_timer.dart完成

--- date_timer.dart
+++ date_timer.dart
@@ -2,28 +2,53 @@ import 'dart:io';
 
 String? order;
 late DateTime starttime;
+late DateTime stoptime;
+bool timesate = false;
 
 void start(){
-  print('カウント開始');
   starttime = DateTime.now();
+  timesate = true;
+  print('カウント開始');
 }
 
 void stop(){
-      var stoptime = DateTime.now();
-      var passtime = stoptime.difference(starttime);
-      print('経過時間: ${passtime.inSeconds} 秒');
+  stoptime = DateTime.now();
+  timesate = false;
+  print('カウント停止');
+}
+
+void passtime(){
+  if(timesate){
+    var nowtime = DateTime.now();
+    var passtime = nowtime.difference(starttime);
+    print('経過時間: ${passtime.inSeconds} 秒');
+  }else if(!timesate){
+    var passtime = stoptime.difference(starttime);
+    print('経過時間: ${passtime.inSeconds} 秒');
+  }
+}
+
+void reset(){
+  starttime = DateTime.now();
+  stoptime = DateTime.now();
+  print('タイマーをリセットしました');
 }
 
 void main(){
-  print("ストップウォッチを開始するならsを押してね");
+  print("ストップウォッチはstartとstopで制御できるよ");
   print('経過時間を知りたいときはtを押してね');
+  print('resetを押すとタイマーがリセットされるよ');
   print('終了するときはendを押してね');
   while(true){
     String? order = stdin.readLineSync();
-    if(order == 's'){
+    if(order == 'start'){
       start();
-    }else if(order == 't'){
+    }else if(order == 'stop'){
       stop();
+    }else if(order == 't'){
+      passtime();
+    }else if(order == 'reset'){
+      reset();
     }else if(order == 'end'){
       print('終了');
       exit(0); 
